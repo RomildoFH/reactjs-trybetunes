c4ba08c74827e10b4a33cd669790d25ee01f4a31
Romildo Silva Filho <romildo.silvafilho1@gmail.com>
2022-10-03 19:01:53 -0300

    Inserida validação do input de pesquisa de artista com habilitação do botão

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -3,13 +3,54 @@ import Header from '../components/Header';
 import LinksBar from '../components/LinksBar';
 
 class Search extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      searchFor: '',
+      disabled: true,
+    };
+  }
+
+  inputValidation = (value) => {
+    const minLength = 2;
+    if (value.length >= minLength) {
+      this.setState({
+        disabled: false,
+      });
+    } else {
+      this.setState({
+        disabled: true,
+      });
+    }
+  };
+
+  handleChange = ({ target }) => {
+    const { value } = target;
+    this.setState({
+      searchFor: value,
+    }, this.inputValidation(value));
+  };
+
   render() {
+    const { searchFor, disabled } = this.state;
     return (
       <div data-testid="page-search">
         <LinksBar />
         <Header />
-        <input type="text" placeholder="Album or Artist Name" />
-        <button type="button">Search</button>
+        <input
+          type="text"
+          placeholder="Album or Artist Name"
+          data-testid="search-artist-input"
+          value={ searchFor }
+          onChange={ this.handleChange }
+        />
+        <button
+          type="button"
+          data-testid="search-artist-button"
+          disabled={ disabled }
+        >
+          Pesquisar
+        </button>
       </div>
 
     );
